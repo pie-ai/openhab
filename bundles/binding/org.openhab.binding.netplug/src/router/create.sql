4df5bebea259db14e583248ecc42e3cd0de7aedd
Patrick Stricker <github.com@pa2.de>
2014-01-21 22:50:25 +0100

    fixed sql create statement

--- bundles/binding/org.openhab.binding.netplug/src/router/create.sql
+++ bundles/binding/org.openhab.binding.netplug/src/router/create.sql
@@ -1,3 +1,13 @@
+CREATE TABLE `commands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `device` int(11) DEFAULT NULL,
+  `command` varchar(64) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `executed_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
 CREATE TABLE `devices` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `netplug_id` varchar(64) DEFAULT NULL,
@@ -5,14 +15,13 @@ CREATE TABLE `devices` (
   `secret` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `netplug_id_UNIQUE` (`netplug_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `commands` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `device` int(11) DEFAULT NULL,
-  `command` varchar(64) DEFAULT NULL,
+  `status` varchar(64) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `executed_at` timestamp NULL DEFAULT NULL,
+  `retrieved_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
